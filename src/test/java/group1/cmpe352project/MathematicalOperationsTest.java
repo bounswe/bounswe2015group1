69ee74c6e031dbdc0dce13884c815b86ad0bc76c
Pinar Sigin <pnrsgn@gmail.com>
2015-05-03 18:11:39 +0300

    TestMinusAdded
    Minus tests are added.

--- src/test/java/group1/cmpe352project/MathematicalOperationsTest.java
+++ src/test/java/group1/cmpe352project/MathematicalOperationsTest.java
@@ -18,17 +18,26 @@ public class MathematicalOperationsTest{
     public void testZeroPlus(){
         assertEquals(2, mo.plus(0,2));
     }
-<<<<<<< HEAD
+
+    @Test
+    public void testSevenMinusNine(){
+        assertEquals(-2, mo.minus(7,9));
+    }
+
+    @Test
+    public void testFourMinusZero(){
+        assertEquals(4, mo.minus(4,0));
+    }
+
     @Test
     public void testThreeTimesFour(){
         assertEquals(12, mo.multiply(3,4));
     }
-    puplic void testNegativeTimesPositive(){
-    
-	assertEquals(-3,mo.multiply(-1,3);	
+
+    @Test
+    public void testNegativeTimesPositive(){
+	   assertEquals(-3,mo.multiply(-1,3);	
     }
-}
-=======
 
     @Test
     public void testFourRemainderThree(){
@@ -39,5 +48,6 @@ public class MathematicalOperationsTest{
     public void testSevenRemainderFive(){
         assertEquals(2, mo.remainder(7,5));
     }
+    
 }
->>>>>>> origin/master
+
