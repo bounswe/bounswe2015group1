23de1b554682dbb25c9dacf20a66e0fd86d5ec2a
onurmus <aztech_onur@hotmail.com>
2015-12-01 00:24:19 +0200

    restaurant register added

--- frontend/app/RegisterCtrl.js
+++ frontend/app/RegisterCtrl.js
@@ -3,5 +3,8 @@ myApp.controller('RegisterCtrl', function($scope, userService) {
 			userService.register($scope.nameRegister, $scope.emailRegister,
 				$scope.passRegister, $scope.birthDateRegister, $scope.locationRegister);
 		}
-
+		$scope.registerAsRestaurant= function(){
+			userService.register($scope.restNameRegister, $scope.restEmailRegister,
+				$scope.resPassRegister, "31.12.2099", $scope.resLocationRegister);
+		}
 	});
