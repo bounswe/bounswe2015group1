63b2405c0fa77f8417ef60ffddf2259f428dd811
Akın Toksan <akintoksan@gmail.com>
2015-12-06 21:29:53 +0200

    Implement an Expandable  RecyclerView for Menu - to be continued

---
+++ WeAreWhatWeEat/app/src/main/java/com/boun/swe/wawwe/Fragments/MenuChild.java
@@ -0,0 +1,22 @@
+package com.boun.swe.wawwe.Fragments;
+
+import java.util.Date;
+
+/**
+ * Created by akintoksan on 06/12/15.
+ */
+
+public class MenuChild {
+
+    private Date mDate;
+    private boolean mSolved;
+
+    public MenuChild(Date date, boolean solved) {
+        mDate = date;
+        mSolved = solved;
+    }
+
+    /**
+     * Getter and setter methods
+     */
+}
